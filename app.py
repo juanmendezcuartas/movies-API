d690287a64f5de6089740207ecebfdf35f55f70d
Juan Mendez <juanmendezcuartas@gmail.com>
2021-06-13 19:13:17 -0500

    Clean some code

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from movies import movies
 
 app = Flask(__name__)
 
+
 @app.route('/ping', methods=['GET'])
 def ping():
   return jsonify({"message": "pong!"})
--- moviesScraping.py
+++ moviesScraping.py
@@ -1,17 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-from lxml import etree, html
 import pandas as pd
-
-url = 'https://www.imdb.com/what-to-watch/popular/?ref_=hm_watch_pop'
-page = requests.get(url)
+#url = 'https://www.imdb.com/what-to-watch/popular/?ref_=hm_watch_pop'
+url = 'https://www.imdb.com'
+page = requests.get(url, timeout=10)
 soup = BeautifulSoup(page.content, 'html.parser')
-movieName = soup.find_all('span', "title")
-#dom = etree.HTML(str(soup))
-#mov = (dom.xpath('//*[@id="__next"]/main/div[2]/section/section/section/div[2]/div/div[1]/div/div[2]/a/span'))
-#tree = html.fromstring(page.content)
-#mov = tree.xpath('//*[@id="__next"]/main/div[2]/section/section/section/div[2]/div/div[1]/div/div[2]/a')
 
-
-for m in movieName:
-    print(m)
+print(soup.find("title").string)
+print(soup.p.string)
